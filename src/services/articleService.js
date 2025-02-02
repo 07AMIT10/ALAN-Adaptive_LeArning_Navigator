20018651d961eed3232865453abf4302457f5a3e
Amit <68996330+07AMIT10@users.noreply.github.com>
2025-02-03 00:16:15 +0530

    Update articleService.js

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -1,11 +1,10 @@
 // src/services/articleService.js
 export async function getRelevantArticles(topic) {
-    // const api = AIzaSyAz3C - Mxg93IRCLL - B1VGAkqbuGFF5kTq8
-    // const c = 250652494c48b4c6b
+    
     try {
         const response = await fetch(
-            // `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_SEARCH_KEY}&cx=${process.env.REACT_APP_CSE_ID}&q=${encodeURIComponent(topic)}&num=3`
-            `https://www.googleapis.com/customsearch/v1?key=AIzaSyAz3C-Mxg93IRCLL-B1VGAkqbuGFF5kTq8&cx=250652494c48b4c6b&q=${encodeURIComponent(topic)}&num=3`
+            `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_SEARCH_KEY}&cx=${process.env.REACT_APP_CSE_ID}&q=${encodeURIComponent(topic)}&num=3`
+            
         );
         const data = await response.json();
         console.log("Article API response:", data);
